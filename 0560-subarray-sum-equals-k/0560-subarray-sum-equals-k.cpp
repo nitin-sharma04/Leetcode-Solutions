ee66f0920411abc305d96c4aeaead1ff9520a8ae
Nitin Sharma <nitinpandit0412@outlook.com>
2024-07-12 01:11:39 +0530

    Time: 55 ms (91.41%), Space: 45.4 MB (48%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,18 +4,18 @@ public:
          unordered_map<int, int> sum_count;
         sum_count[0] = 1;
         int sum = 0;
-        int maxlen = 0;
+        int total_subArray = 0;
 
         for (int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
 
             if (sum_count.find(sum - k) != sum_count.end()) {
-                maxlen += sum_count[sum - k];
+                total_subArray += sum_count[sum - k];
             }
 
             sum_count[sum]++;
         }
 
-        return maxlen;
+        return total_subArray;
     }
 };
