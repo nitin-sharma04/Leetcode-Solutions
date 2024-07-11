d1ab93266bedee19983473080756b289af8da184
Nitin Sharma <nitinpandit0412@outlook.com>
2024-07-12 00:35:58 +0530

    Time: 62 ms (77.03%), Space: 45.4 MB (47.95%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+         unordered_map<int, int> sum_count;
+        sum_count[0] = 1;
+        int sum = 0;
+        int maxlen = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+
+            if (sum_count.find(sum - k) != sum_count.end()) {
+                maxlen += sum_count[sum - k];
+            }
+
+            sum_count[sum]++;
+        }
+
+        return maxlen;
+    }
+};
