cb4967a2a02d0a322360ab5318deedbe9db9a927
Nitin Sharma <nitinpandit0412@outlook.com>
2024-07-14 04:53:00 +0530

    Time: 3 ms (88.64%), Space: 27.1 MB (12.11%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        for(int i=1;i<nums.size();i++){
+            if((nums[i]&1)&&(nums[i-1]&1))
+                return false;
+            if(!(nums[i]&1)&&!(nums[i-1]&1))
+                return false;     
+        }
+        return true;
+    }
+};
