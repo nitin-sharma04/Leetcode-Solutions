faf7fdcd129a74725f92a7e84ce990a068ea5238
Nitin Sharma <nitinpandit0412@outlook.com>
2024-09-10 23:38:46 +0530

    Time: 4 ms (39.68%), Space: 9 MB (60.1%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,13 +3,36 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>>rotatematrix(n,(vector<int>(m)));
+
+        //transpose matrix
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<m;j++){
+                 swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+
+        //reverse the row to perform 90 deg rotation 
+        for( int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
+
+/*
+Brute solution 
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>rotatematrix(n,(vector<int>(m))); //2d new matrix for rotate 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 rotatematrix[j][n-1-i]=matrix[i][j];
             }
         }
-        matrix=rotatematrix;
+        matrix=rotatematrix; // save into original 
     }
 
 };
+*/
