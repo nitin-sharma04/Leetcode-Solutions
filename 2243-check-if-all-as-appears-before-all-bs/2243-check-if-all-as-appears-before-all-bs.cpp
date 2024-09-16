799f715cc277762b6f778b9fc9077212a9b3a1d8
Nitin Sharma <nitinpandit0412@outlook.com>
2024-09-16 23:20:11 +0530

    Time: 4 ms (17.13%), Space: 7.7 MB (22.16%) - LeetHub

---
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        for(int i=1;i<s.size();i++){
+            if(s[i]<s[i-1])
+                return false;
+        }
+       return true; 
+    }
+};
