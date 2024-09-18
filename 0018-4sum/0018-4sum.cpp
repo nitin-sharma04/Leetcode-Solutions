55843a31d0782941e71d85e4c361455d2665a116
Nitin Sharma <nitinpandit0412@outlook.com>
2024-09-18 11:26:37 +0530

    Time: 29 ms (44.85%), Space: 16.2 MB (74.61%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end()); // Sort the array to use two-pointer approach
+        sort(nums.begin(), nums.end()); 
         vector<vector<int>> ans;
         
         for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Avoid duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue; 
             
             for (int j = i + 1; j < nums.size(); j++) {
-                if (j != i + 1 && nums[j] == nums[j - 1]) continue; // Avoid duplicates for the second number
-                
+                if (j != i + 1 && nums[j] == nums[j - 1]) continue; 
+            
                 int k = j + 1;
                 int l = nums.size() - 1;
                 
                 while (k < l) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l]; // Handle overflow using long long
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
                     
                     if (sum == target) {
                         ans.push_back({nums[i], nums[j], nums[k], nums[l]});
-                        
-                        // Avoid duplicates for the third number
                         while (k < l && nums[k] == nums[k + 1]) k++;
-                        // Avoid duplicates for the fourth number
                         while (k < l && nums[l] == nums[l - 1]) l--;
-                        
                         k++;
                         l--;
                     } else if (sum < target) {
