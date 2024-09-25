7fb890c2df90d47c937355a7017809853ec75dda
Nitin Sharma <nitinpandit0412@outlook.com>
2024-09-26 01:46:36 +0530

    Time: 0 ms (100%), Space: 7.8 MB (85.11%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int>res;
+        long long ans=1;
+        res.push_back(ans);
+        for(int i=1;i<=rowIndex;i++){
+           ans = ans * (rowIndex - i + 1) / i;
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
