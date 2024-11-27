c130a4b1d203dba5c00675bae091f1a0f9a8b240
Nitin Sharma <nitinpandit0412@outlook.com>
2024-11-28 00:20:36 +0530

    Time: 7 ms (72.18%), Space: 140.8 MB (5.58%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -3,25 +3,24 @@ public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         vector<int> result, less, equal, high;
 
-        for (auto num: nums) {
-            if (num < pivot)
-                less.push_back(num);
+        for (auto it : nums) {
+            if (it < pivot)
+                less.push_back(it);
         }
 
-        for (auto num:nums) {
-            if (num == pivot)
-                equal.push_back(num);
+        for (auto it : nums) {
+            if (it == pivot)
+                equal.push_back(it);
         }
 
-        for (auto num:nums) {
-            if (num > pivot)
-                high.push_back(num);
+        for (auto it : nums) {
+            if (it > pivot)
+                high.push_back(it);
         }
 
         result.insert(result.end(), less.begin(), less.end());
         result.insert(result.end(), equal.begin(), equal.end());
         result.insert(result.end(), high.begin(), high.end());
-
-            return result;
+        return result;
     }
 };
