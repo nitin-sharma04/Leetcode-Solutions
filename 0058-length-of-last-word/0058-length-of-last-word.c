f2e1b40373c0b4405a88cbe7da7ce5c0b5c4d402
Nitin Sharma <nitinpandit0412@outlook.com>
2025-07-16 00:49:39 +0530

    Time: 0 ms (100%), Space: 8 MB (76.85%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.c
+++ 0058-length-of-last-word/0058-length-of-last-word.c
@@ -1,15 +1,15 @@
 int lengthOfLastWord(char* s) {
     int length = 0;
     int i = strlen(s) - 1;
-
-    while (i >= 0 && isspace(s[i])) {
+    
+    while (i >= 0 && s[i] == ' ') {
         i--;
     }
-
-    while (i >= 0 && !isspace(s[i])) {
+    
+    while (i >= 0 && s[i] != ' ') {
         length++;
         i--;
     }
-
+    
     return length;
 }
